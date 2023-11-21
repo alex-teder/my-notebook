21f32072e516140ebcbce2c390987f1900e1899f
Aleksandr Teder <tederkedr@gmail.com>
2023-11-21 16:30:56 +0400

    expanded MyCard props and styling

--- src/components/ui/MyCard.jsx
+++ src/components/ui/MyCard.jsx
@@ -1,8 +1,18 @@
 import s from './MyCard.module.scss'
 
-export function MyCard({children, width = 'unset', height = 'unset'}) {
+export function MyCard({children, color, rounded = false, width = 'unset', height = 'unset'}) {
+  const style = {
+    width,
+    height,
+  }
+
+  if (color) {
+    style.borderLeft = `10px solid ${color}`
+    style.borderRight = `10px solid ${color}`
+  }
+
   return (
-    <div className={s.card} style={{width, height}}>
+    <div className={rounded ? s.rounded : s.card} style={style}>
       {children}
     </div>
   )
