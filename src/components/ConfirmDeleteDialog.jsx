3abb1e55a255532e09a41062a739e1c124094b3d
Aleksandr Teder <tederkedr@gmail.com>
2023-11-21 20:45:36 +0400

    added ConfirmDeleteDialog

---
+++ src/components/ConfirmDeleteDialog.jsx
@@ -0,0 +1,19 @@
+import {MyDialog} from './ui/MyDialog'
+import {MyCard} from './ui/MyCard'
+import {MyButton} from './ui/MyButton'
+import s from './ConfirmDeleteDialog.module.scss'
+
+export function ConfirmDeleteDialog({note, close}) {
+  return (
+    <MyDialog>
+      <MyCard>
+        <h4>Are you sure you want to delete this note?</h4>
+        <p className={s.noteTitle}>{note.title}</p>
+        <div className={s.actions}>
+          <MyButton onClick={close}>Cancel</MyButton>
+          <MyButton onClick={close}>Confirm</MyButton>
+        </div>
+      </MyCard>
+    </MyDialog>
+  )
+}
--- src/components/notes/NoteItem/index.jsx
+++ src/components/notes/NoteItem/index.jsx
@@ -3,20 +3,26 @@ import {MyCard} from '../../ui/MyCard'
 import {NoteItemContent} from './NoteItemContent'
 import {NoteItemDetails} from './NoteItemDetails'
 import {NoteItemActions} from './NoteItemActions'
+import {ConfirmDeleteDialog} from '../../ConfirmDeleteDialog'
 
 export function NoteItem({note}) {
   const [isExpanded, setIsExpanded] = useState(false)
+  const [isDeleteDialog, setIsDeleteDialog] = useState(false)
 
   return (
-    <MyCard color={note.color}>
-      <NoteItemContent note={note} isExpanded={isExpanded} />
-      <NoteItemDetails note={note} />
-      <NoteItemActions
-        onEdit={() => {}}
-        onDelete={() => {}}
-        isExpanded={isExpanded}
-        onExpand={() => setIsExpanded(v => !v)}
-      />
-    </MyCard>
+    <>
+      <MyCard color={note.color}>
+        <NoteItemContent note={note} isExpanded={isExpanded} />
+        <NoteItemDetails note={note} />
+        <NoteItemActions
+          onEdit={() => {}}
+          onDelete={() => setIsDeleteDialog(true)}
+          isExpanded={isExpanded}
+          onExpand={() => setIsExpanded(v => !v)}
+        />
+      </MyCard>
+
+      {isDeleteDialog && <ConfirmDeleteDialog note={note} close={() => setIsDeleteDialog(false)} />}
+    </>
   )
 }
