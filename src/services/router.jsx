8c609a2265be8144735c0858157a4b31b96c205c
Aleksandr Teder <tederkedr@gmail.com>
2023-11-29 20:37:27 +0400

    added route protection via loader method

--- src/services/router.jsx
+++ src/services/router.jsx
@@ -3,33 +3,51 @@ import {LoginPage} from '/src/pages/LoginPage'
 import {PublicNotesPage} from '/src/pages/PublicNotesPage'
 import {PersonalNotesPage} from '/src/pages/PersonalNotesPage'
 import {SettingsPage} from '/src/pages/SettingsPage'
-import {SingleNotePage} from '../pages/SingleNotePage'
+import {SingleNotePage} from '/src/pages/SingleNotePage'
+import {getItem} from '/src/utils/storageUtils'
+
+const checkUser = () => !!getItem('user')
+
+const requiresAuth = () => {
+  if (!checkUser()) return redirect('/login')
+  return null
+}
+const requiresNoAuth = () => {
+  if (checkUser()) return redirect('/')
+  return null
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
     loader() {
+      if (checkUser()) return redirect('/personal')
       return redirect('/login')
     },
   },
   {
     path: '/login',
     element: <LoginPage />,
+    loader: requiresNoAuth,
   },
   {
     path: '/public',
     element: <PublicNotesPage />,
+    loader: requiresAuth,
   },
   {
     path: '/personal',
     element: <PersonalNotesPage />,
+    loader: requiresAuth,
   },
   {
     path: '/settings',
     element: <SettingsPage />,
+    loader: requiresAuth,
   },
   {
     path: '/note/:noteId',
     element: <SingleNotePage />,
+    loader: requiresAuth,
   },
 ])
