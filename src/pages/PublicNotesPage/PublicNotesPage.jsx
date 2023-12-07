960412a6d2cdfad0cd9cc6fb7ce8fff9bc539c4c
Aleksandr Teder <tederkedr@gmail.com>
2023-12-07 15:25:36 +0400

    added fetching public notes

--- src/pages/PublicNotesPage/PublicNotesPage.jsx
+++ src/pages/PublicNotesPage/PublicNotesPage.jsx
@@ -1,6 +1,6 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import {useLocale} from '/src/hooks/useLocale'
 import {MyCheckbox} from '/src/components/ui/MyCheckbox'
 import {NoteList} from '/src/components/notes/NoteList'
@@ -8,21 +8,35 @@ import {MainLayoutWrapper} from '/src/components/layout/MainLayoutWrapper'
 import {getItem} from '/src/utils/storageUtils'
 import {PATHS} from '/src/services/router'
 import s from './PublicNotesPage.module.scss'
+import {fetchPublicNotes} from '../../store/publicNotes'
 
 export function PublicNotesPage() {
   const {$t} = useLocale()
-  const notes = useSelector(state => state.publicNotes)
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state.publicNotes.notes)
+  const status = useSelector(state => state.publicNotes.status)
+  const token = useSelector(state => state.user.token)
   const [favFilter, setFavFilter] = useState(false)
   const filteredNotes = notes.filter(
     note => getItem('user').favorites && getItem('user').favorites.includes(note.id)
   )
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchPublicNotes(token))
+    }
+  }, [dispatch, status, token])
 
   return (
     <MainLayoutWrapper>
       <Heading />
       <NavBar />
       <MyCheckbox label={$t('only_fav')} value={favFilter} onChange={() => setFavFilter(v => !v)} />
-      <NoteList notes={favFilter ? filteredNotes : notes} favable />
+      {status === 'loading' ? (
+        <p>Getting data...</p>
+      ) : (
+        <NoteList notes={favFilter ? filteredNotes : notes} favable />
+      )}
     </MainLayoutWrapper>
   )
 }
--- src/store/publicNotes.js
+++ src/store/publicNotes.js
@@ -1,11 +1,45 @@
-import {createSlice} from '@reduxjs/toolkit'
-import {mockNotes} from '/src/utils/mockNotes'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
-const initialState = mockNotes
+const STATUS_ENUM = {
+  IDLE: 'idle',
+  LOADING: 'loading',
+  SUCCEEDED: 'succeeded',
+  FAILED: 'failed',
+}
+
+const initialState = {
+  notes: [],
+  status: STATUS_ENUM.IDLE,
+  error: null,
+}
+
+export const fetchPublicNotes = createAsyncThunk('publicNotes/fetchPublicNotes', async token => {
+  const URL = 'https://dull-pear-haddock-belt.cyclic.app/notes?type=public'
+  const response = await fetch(URL, {
+    method: 'GET',
+    headers: {Authorization: `Bearer: ${token}`},
+  })
+  const data = await response.json()
+  return data
+})
 
 const publicNotesSlice = createSlice({
   name: 'publicNotes',
   initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPublicNotes.pending, state => {
+        state.status = STATUS_ENUM.LOADING
+      })
+      .addCase(fetchPublicNotes.fulfilled, (state, action) => {
+        state.status = STATUS_ENUM.SUCCEEDED
+        state.notes = action.payload
+      })
+      .addCase(fetchPublicNotes.rejected, (state, action) => {
+        state.status = STATUS_ENUM.FAILED
+        state.error = action.error.message
+      })
+  },
 })
 
 export const publicNotesReducer = publicNotesSlice.reducer
