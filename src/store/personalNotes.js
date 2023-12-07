53f0f8596338861f18793e08503057c5528b0599
Aleksandr Teder <tederkedr@gmail.com>
2023-12-07 15:39:04 +0400

    added fetching personal notes

--- src/store/personalNotes.js
+++ src/store/personalNotes.js
@@ -1,7 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit'
-import {mockNotes} from '/src/utils/mockNotes'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
-const initialState = mockNotes
+const STATUS_ENUM = {
+  IDLE: 'idle',
+  LOADING: 'loading',
+  SUCCEEDED: 'succeeded',
+  FAILED: 'failed',
+}
+
+const initialState = {
+  notes: [],
+  status: STATUS_ENUM.IDLE,
+  error: null,
+}
+
+export const fetchPersonalNotes = createAsyncThunk(
+  'personalNotes/fetchPersonalNotes',
+  async token => {
+    const URL = 'https://dull-pear-haddock-belt.cyclic.app/notes?type=personal'
+    const response = await fetch(URL, {
+      method: 'GET',
+      headers: {Authorization: `Bearer: ${token}`},
+    })
+    const data = await response.json()
+    return data
+  }
+)
 
 const personalNotesSlice = createSlice({
   name: 'personalNotes',
@@ -17,6 +40,20 @@ const personalNotesSlice = createSlice({
     deleteNote(state, action) {
       const index = state.findIndex(({id}) => id === action.payload.id)
       state.splice(index, 1)
+    },
+    extraReducers(builder) {
+      builder
+        .addCase(fetchPersonalNotes.pending, state => {
+          state.status = STATUS_ENUM.LOADING
+        })
+        .addCase(fetchPersonalNotes.fulfilled, (state, action) => {
+          state.status = STATUS_ENUM.SUCCEEDED
+          state.notes = action.payload
+        })
+        .addCase(fetchPersonalNotes.rejected, (state, action) => {
+          state.status = STATUS_ENUM.FAILED
+          state.error = action.error.message
+        })
     },
   },
 })
